5709ffa3220134d4dbaf6e4edc631633a4c6f6ee
lex0127316 <invinciblemadara50@gmail.com>
2025-11-01 16:36:13 +0100

    chore: env setup for next in web page

---
+++ frontend/web/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+    turbo: true
+  }
+};
+
+export default nextConfig;
